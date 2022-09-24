cf628f63ca4d4bc6689bd8199881269776703c48
Naveen1k <111062137+Naveen1k@users.noreply.github.com>
2022-09-24 11:14:45 +0530

    Co-authored-by: Knightsaket007 <Knightsaket007@users.noreply.github.com>

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+function Home(){
+    return(
+        <>
+        <Navbar/>
+        
+        
+
+        {/* <img src="/ReactEvento/images/Pic.jpg"></img> */}
+        </>
+    )
+} 
+export default Home;
