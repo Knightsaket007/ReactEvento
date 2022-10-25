9cdb31e581382d21b237ee08e47bc0861e589bb6
Naveen1k <111062137+Naveen1k@users.noreply.github.com>
2022-10-25 15:50:21 +0530

    Co-authored-by: Knightsaket007 <Knightsaket007@users.noreply.github.com>

---
+++ src/Pages/Components/Ecard.js
@@ -0,0 +1,129 @@
+
+import './Ecards.css'
+function Ecards() {
+  // VanillaTilt.init(document.querySelectorAll(".card"),{
+  //     glare: true,
+  //     reverse: true,
+  //     "max-glare": 0.15
+  //   });
+  return (
+    <>
+
+      <div className='card-container' >
+
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+
+
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+
+            <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+
+            <button className='btn'>Buy Now</button>
+
+          </div>
+        </div>
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+            <button>Buy Now</button>
+          </div>
+        </div>
+
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+            <button>Buy Now</button>
+          </div>
+        </div>
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+            <button>Buy Now</button>
+          </div>
+        </div>
+
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+            <button>Buy Now</button>
+          </div>
+        </div>
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+            <button>Buy Now</button>
+          </div>
+        </div>
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+            <button>Buy Now</button>
+          </div>
+        </div>
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+            <button>Buy Now</button>
+          </div>
+        </div>
+        <div className='card-sub-container'>
+          <div className='card-head'>
+            <img className='card-img' alt='event' src='../ReactEvento/images/gaming.jpg'></img>
+          </div>
+          <div className='card-body'><h1>Hello</h1>
+            <p>details</p>
+            <button>Buy Now</button>
+          </div>
+        </div>
+
+
+      </div>
+
+      
+
+      <img className='img-ended-event' src="../ReactEvento/images/fest.svg" />
+      
+    <img className='img-ended-park' src="../ReactEvento/images/park.svg" />
+
+
+    
+      </>
+
+      )
+}
+
+      export default Ecards
---
+++ src/Pages/Events.js
@@ -0,0 +1,27 @@
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+// import Cards from "./Components/Cards";
+import Ecards from "./Components/Ecard";
+import {BsSearch} from 'react-icons/bs'
+
+function Events() {
+    return (
+        <>
+            <Navbar />
+
+            {/* searchBar */}
+
+            <div className="search">
+                <input className="search-bar" type='search'  />
+                <button className="Search-icon"><BsSearch/></button>
+            </div>
+            <img className="img-back-top" src="./ReactEvento/images/coolBoy.png" />
+
+            {/* <Cards/> */}
+            <Ecards />
+            <Footer />
+        </>
+    )
+}
+
+export default Events;
