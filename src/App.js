a78861d1d8226ee45cfb7f1dd696873aa9a09fda
Naveen1k <111062137+Naveen1k@users.noreply.github.com>
2022-12-23 12:51:38 +0530

    UI design
    Co-authored-by: Knightsaket007 <Knightsaket007@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Events from "./Pages/Events";
 import Home from "./Pages/Home";
-import Login from "./Pages/User_Login"
+import UserSignup from "./Pages/UserSignup"
+import UserLog from "./Pages/UserLog"
 import Aboutus from "./Pages/Aboutus"
+import EventInfo from "./Pages/EventInfo"
 let App = () => {
 
   return (
@@ -12,8 +14,10 @@ let App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Events" element={<Events />}></Route>
-          <Route path="/login" element={<Login/>}></Route>
           <Route path="/about" element={<Aboutus/>}></Route>
+          <Route path="/signup" element={<UserSignup/>}></Route>
+          <Route path="/login" element={<UserLog/>}></Route>
+          <Route path="/eventinfo" element={<EventInfo/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/Pages/Components/Cards.js
+++ src/Pages/Components/Cards.js
@@ -1,10 +1,108 @@
 import './Cards.css'
+import React, { Component } from 'react';
+import Info from "./Info.json"
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
 function Cards() {
+ 
+ 
+  //   const [Info, setInfo] = useState([]);
+
+  //   useEffect(() => {t [Price,setPrice] = useState('');
+  // const [Title,setTitle] = useState('');
+ 
+  //       getAPI("./Info.json")    
+  //   }, []); 
 
 
-  return (
+  // cons // const [Image,setImage] = useState('');
+  // const [Seats,setSeats] = useState('');
+
+
+
+  // const getAPI=(data)=>{
+  //   // console.log(data)
+
+  //   fetch(data, {
+  //     headers:
+  //     {
+  //         'Content-Type': 'application/json',
+  //         'Accept': 'application/json'
+  //     }
+  // })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       // console.log("hello")
+  //         console.log(data)
+  //         setInfo(data);
+  //     })
+  // }
+  // // useEffect(()=>{
+  // //   const fun=()=>{
+  // //   axios.get('./Info.json').then((res)=>{
+  // //     console.log(res.data)
+  // //   })
+  // // }
+  // // fun();
+
+  // // },[])
+
+
+
+
+  return (   
     <>
+    <div className="main-card-container">
+      {
+        Info.map((value,index) => {
+          return (
+            <>
+              
+
+              <div className='card-body'>
+          <div className='card-img'>
+            <img className='pure-img' src="/ReactEvento/images/seat.jpg" alt="wait" ></img>
+          </div>
+          <div className='card-content'>
+          <div className='title-div'> 
+             <p className='head-para'><Link to='/' style={{ textDecoration: "none" }}> {value.Title}</Link> </p>
+          </div>
+
+            <h2 >₹100</h2>
+            <h3 className='price-cut'>₹150</h3>
+            <h4 className='seats'>Seats remain:<span> 000</span> </h4>
+
+          </div>
+        </div>
+
+        {/* Mobile */}
+
+        <div className='mob-card-body'>
+          <div className='mob-img-div'>
+            <img className='pureMob-img' src="/ReactEvento/images/seat.jpg" alt="wait" ></img>
+          </div>
+          <div className='mob-content'>
+            <p className='mob-para'><Link to='/' style={{ textDecoration: "none" }}> HELLO- world this is my product and you have to buy jhkjh s jhwg deh hbdhjgwjdg b...</Link> </p>
+            <h2 className='price'>₹100  <span className='mob-price-cut'>₹150</span></h2>
+
+            <h4 className='mob-seats'>Seats remain:<span> 000</span> </h4>
+            {/* </div> */}
+          </div>
+        </div>
+
+              
+
+            </>
+
+          )
+        })
+      }
+      </div>
+
+
       <div className="main-card-container">
 
         <div className='card-body'>
@@ -77,7 +175,7 @@ function Cards() {
           </div>
         </div>
 
-         <div className='card-body'>
+        <div className='card-body'>
           <div className='card-img'>
             <img className='pure-img' src="/ReactEvento/images/seat.jpg" alt="wait" ></img>
           </div>
@@ -107,7 +205,7 @@ function Cards() {
           </div>
         </div>
 
-            
+
         <div className='mob-card-body'>
           <div className='mob-img-div'>
             <img className='pureMob-img' src="/ReactEvento/images/seat.jpg" alt="wait" ></img>
